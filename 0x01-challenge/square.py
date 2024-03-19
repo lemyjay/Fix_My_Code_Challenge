dc5da09efac3d5f808e31fe01184b5f82e050cee
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-03-19 13:25:37 +0000

    some minor changes

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -16,7 +16,7 @@ class Square():
         """ Area of the square """
         return self.height * self.width
 
-    def PermiterOfMySquare(self):
+    def permiter_of_square(self):
         '''Calculates and returns the perimeter of a square'''
         return (self.width * 2) + (self.height * 2)
 
@@ -29,4 +29,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_square())
