12c3c4892db3499815e64fbd8ea208fd63002cf9
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-03-19 13:15:04 +0000

    pycodestyle

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -7,7 +7,6 @@ class square():
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         '''For initializing an object of the class'''
         for key, value in kwargs.items():
